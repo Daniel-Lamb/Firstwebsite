aa9050c2dd207676f23f59262b81cc535eee6492
Daniel Lamb <96439440+Daniel-Lamb@users.noreply.github.com>
2023-06-27 01:43:38 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
   
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Danielamb</h1>
       </div>
