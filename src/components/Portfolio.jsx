d17a4fbb41ebddbd2945e2e7d5799f3565841a6f
Daniel Lamb <96439440+Daniel-Lamb@users.noreply.github.com>
2023-06-27 02:32:48 -0400

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -43,7 +43,7 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6">Check out some cool projects I've done</p>
+          <p className="py-6">Check out some cool projects I've worked on</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
@@ -56,10 +56,7 @@ const Portfolio = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                  Learn More
                 </button>
               </div>
             </div>
