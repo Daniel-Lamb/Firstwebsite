185dc5798f90fee611d2f7b3c4ee83b0962ecbed
Daniel Lamb <96439440+Daniel-Lamb@users.noreply.github.com>
2023-06-27 02:40:50 -0400

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,6 +11,7 @@ const Portfolio = () => {
     {
       id: 1,
       src: arrayDestruct,
+      link: https://google.com,
     },
     {
       id: 2,
@@ -47,7 +48,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, link }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -55,10 +56,8 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <a href="https://example.com">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Learn More
-                  </button>
+                <a href={link} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Learn More
                 </a>
               </div>
             </div>
